230d46257313e92be2498deab955dfa9754a94e7
naresh <naresh.devops31@gmail.com>
2017-07-29 15:49:36 +0530

    third1

--- src/main/java/sonar/App.java
+++ src/main/java/sonar/App.java
@@ -2,12 +2,12 @@ package sonar;
 
 /**
  * Hello world!
- *
+ *gmai
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "H wre rqrqello World! rigt" );
+        System.out.println( "H wre rqrqello World! rigddsdst" );
     }
 }
