f767522252318820557cd3cc8f72cc23ea832729
naresh <naresh.devops31@gmail.com>
2017-07-29 15:18:10 +0530

    third

--- src/main/java/sonar/App.java
+++ src/main/java/sonar/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "H wre rqrqello World!" );
+        System.out.println( "H wre rqrqello World! rigt" );
     }
 }
